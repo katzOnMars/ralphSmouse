61945480baf852fa760ddb0745b71f649f956b60
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-11-20 20:22:41 -0500

    change to require json

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 const data = require('./response.json');
 
 app.get('/', (req, res) => {
-  console.log(data);
+  console.log(JSON.parse(data));
+  console.log(req);
   res.json(data);
 });
 
