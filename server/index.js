848e5a07607b3b04cdf4687b2a5746b83d334163
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-11-20 19:28:57 -0500

    add console log for request

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const app = require('express')();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.json('hey'));
+app.get('/', (req, res) => {
+  console.log(req);
+  res.json('hey')
+});
 
 app.listen(port, () => console.log(`server up on ${port}`));
