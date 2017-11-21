8f504fb7996c73ba5fb4e956d4f76776086ac95a
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-11-20 20:02:02 -0500

    remove sync

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const port = process.env.PORT || 8080;
 const fs = require('fs');
 
-const data = fs.readFileSync('./response.json');
+const data = fs.readFile('./response.json');
 
 app.get('/', (req, res) => {
   console.log(JSON.parse(data));
