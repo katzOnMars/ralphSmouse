b073e8c9a25d54dc5abf2caff5d83cac8ff4113d
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-11-20 20:20:12 -0500

    change to require json

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const app = require('express')();
 const port = process.env.PORT || 8080;
 const fs = require('fs');
 
-const data = fs.readFile('./server/response.json');
+const data = require('./response.json');
 
 app.get('/', (req, res) => {
-  console.log(JSON.parse(data));
-  res.json(JSON.parse(data))
+  console.log(data);
+  res.json(data);
 });
 
 app.listen(port, () => console.log(`server up on ${port}`));
