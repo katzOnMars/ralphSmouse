f2b5d22f566939358f2145739e3551f556e2f350
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-11-20 19:29:34 -0500

    console log req

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   console.log(req);
-  res.json('hey')
+  res.json(req)
 });
 
 app.listen(port, () => console.log(`server up on ${port}`));
