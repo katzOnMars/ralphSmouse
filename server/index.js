3369610a5b782a3169570aca9aecb5056e942649
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-11-20 19:58:53 -0500

    connect dummy json

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const app = require('express')();
 const port = process.env.PORT || 8080;
+const fs = require('fs');
+
+const data = fs.readFileSync('./response.json');
 
 app.get('/', (req, res) => {
-  console.log(req);
-  res.json('hey')
+  console.log(JSON.parse(data));
+  res.json(JSON.parse(data))
 });
 
 app.listen(port, () => console.log(`server up on ${port}`));
