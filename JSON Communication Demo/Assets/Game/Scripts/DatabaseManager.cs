619a0fda104251e3796fc28bd4368cbf37045a29
davegasparini <davegasparini@users.noreply.github.com>
2017-12-28 17:17:50 -0500

    unity file
    adding/moving unity files

---
+++ JSON Communication Demo/Assets/Game/Scripts/DatabaseManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class DatabaseManager : MonoBehaviour {
+
+	public static DatabaseManager sharedInstance = null;
+	
+	// 
+	void Awake () {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        } else if (sharedInstance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://cats-on-mars.firebaseio.com/");
+        Debug.Log (Router.Players());
+        Router.Players().SetValueAsync("testing 1, 2, 3");
+	}
+
+
+}
